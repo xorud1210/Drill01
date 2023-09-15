06a980baa7f0758ffa7ba1581867c9f1811e74bb
xorud1210 <rhxo1210@naver.com>
2023-09-15 21:16:17 +0900

    다른 방향 함수 및 입력받기 생성

--- Drill01.py
+++ Drill01.py
@@ -1,12 +1,28 @@
 import turtle
 
+def move_right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
 def move_up():
     turtle.setheading(90)
     turtle.forward(50)
     turtle.stamp()
+def move_left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+def move_down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+    
 
 turtle.shape('turtle')
 
 turtle.onkey(move_up,'w')
+turtle.onkey(move_left,'a')
+turtle.onkey(move_down,'s')
+turtle.onkey(move_right,'d')
 turtle.listen()
              
